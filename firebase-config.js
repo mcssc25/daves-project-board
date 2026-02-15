d86c533e28cc1f01ece873b698c6a9fa590925b2
mcssc25 <davedavisre@gmail.com>
2026-02-15 15:46:29 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+﻿// Firebase Web App config for Dave's Project Board.
+// Fill these in from Firebase Console -> Project settings -> Your apps -> Web app config.
+window.DPB_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAE4zGrV60abFkczKrVr2tLX_N86n1AE6s",
+  authDomain: "kanban-9436f.firebaseapp.com",
+  projectId: "kanban-9436f",
+  storageBucket: "kanban-9436f.firebasestorage.app",
+  messagingSenderId: "671405157961",
+  appId: "1:671405157961:web:7554dd522cada698725998"
+};
